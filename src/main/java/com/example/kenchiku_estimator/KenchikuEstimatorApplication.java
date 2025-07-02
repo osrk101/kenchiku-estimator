39c238d4352bdce1edb8a945d7f0480e58f5b160
osrk101 <osrk101@gmail.com>
2025-07-02 11:24:31 +0900

    最初のコミット

---
+++ src/main/java/com/example/kenchiku_estimator/KenchikuEstimatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.kenchiku_estimator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenchikuEstimatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenchikuEstimatorApplication.class, args);
+	}
+
+}
