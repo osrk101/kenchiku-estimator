ca9f019cb83acddc96540ed6808705197e3c3519
osrk101 <osrk101@gmail.com>
2025-09-25 15:51:17 +0900

    account.createを整形中

--- src/main/java/com/kenchiku_estimator/form/AccountForm.java
+++ src/main/java/com/kenchiku_estimator/form/AccountForm.java
@@ -1,6 +1,7 @@
 package com.kenchiku_estimator.form;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -8,15 +9,19 @@ public class AccountForm {
 
   private int id;
 
-  @NotBlank
+  @NotBlank(message = "アカウント名を入力してください")
+  @Size(max = 50, message = "アカウント名は50文字以内で入力してください")
   private String username;
 
+  @NotBlank(message = "パスワードを入力してください")
+  @Size(min = 8, message = "パスワードは8文字以上で入力してください")
   private String password;
 
-  @NotBlank
+  @NotBlank(message = "氏名を入力してください")
+  @Size(max = 50, message = "氏名は50文字以内で入力してください")
   private String fullName;
 
-  @NotBlank
+  @NotBlank(message = "権限を選択してください")
   private String role;
 }
 
